5ff2c824f7c30d00a0c41fefd1ed1118efa5f1bd
Arun.Mandal <Arun.Mandal@CGNPLAP449>
2021-12-03 19:06:27 +0545

    Add project files.

---
+++ CoffeePicker/Server/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoffeePicker.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeePicker.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Coffee> Coffee { get; set; }
+    }
+}
+
+
